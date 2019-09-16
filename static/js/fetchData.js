156767a9a6b78b7b024493353becc1ecd5146382
Saptak S <saptak013@gmail.com>
2019-09-16 19:18:40 +0530

    Add count for same host names with same type

--- static/js/fetchData.js
+++ static/js/fetchData.js
@@ -4,17 +4,42 @@ async function getData() {
   return dns_data;
 }
 
+function getHtmlTemplate(name, type, count) {
+  return `<div class="d-flex data-container">
+      <div id="dns-text">
+        ${name}
+      </div>
+      <div class="dns-count">
+        ${count}
+      </div>
+    </div>
+    <div class="action-buttons d-flex">
+      <button id="block-dns" class="btn-default">Ignore</button>
+      <button id="block-dns" class="btn-danger">Block</button>
+    </div>`;
+}
+
+function removeDuplicateDns(row) {
+  row.parentNode.removeChild(row);
+}
+
 function showData(data) {
   data.forEach(dns => {
     const dnsContainer = document.getElementById('dns-container');
     const dnsRow = document.createElement('div');
-    dnsRow.setAttribute('class', 'row d-flex');
-    dnsRow.innerHTML = `<div id="dns-text">
-        ${dns[0]['Name']}
-      </div>
-      <div class="action-button">
-        <button id="block-dns" class="btn-danger">Block</button>
-      </div>`;
+    dnsRow.setAttribute('class', 'row');
+    dnsRow.setAttribute('data-host', dns[0]['Name']);
+    dnsRow.setAttribute('data-type', dns[0]['Type']);
+    const duplicateDns = document.querySelector(
+      '.row[data-host="' + dns[0]['Name'] + '"][data-type="' + dns[0]['Type'] + '"]'
+    );
+    let dnsCount = "1";
+    if (duplicateDns) {
+      removeDuplicateDns(duplicateDns);
+      dnsCount = Number(duplicateDns.getAttribute('data-count')) + 1;
+    }
+    dnsRow.setAttribute('data-count', dnsCount);
+    dnsRow.innerHTML = getHtmlTemplate(dns[0]['Name'], dns[0]['Type'], dnsCount);
     dnsContainer.appendChild(dnsRow);
   });
 }
