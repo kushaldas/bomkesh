679a8e8d6f5552dff6b1faa92634e0341365401d
Saptak S <saptak013@gmail.com>
2020-07-09 00:11:35 +0530

    Makes server run on 0.0.0.0 instead of localhost

--- manage.py
+++ manage.py
@@ -1,14 +1,16 @@
 import os
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from bomkesh import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
+server = Server(host="0.0.0.0", port=5000)
 
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
+manager.add_command("runserver", server)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
